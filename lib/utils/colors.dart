f7863776f10054735393d61562829c05948b0114
oluwajueda <oluwajuedaloade@gmail.com>
2023-03-07 02:22:45 +0100

    create constant colors in utils

---
+++ lib/utils/colors.dart
@@ -0,0 +1,6 @@
+import "package:flutter/material.dart";
+
+const backgroundColor = Color.fromRGBO(245, 245, 245, 1);
+const buttonColor = Color.fromRGBO(24, 23, 23, 1);
+const textColor = Color.fromRGBO(0, 0, 0, 1);
+const whiteBackground = Color.fromRGBO(255, 255, 255, 1);
